affe88b99ec01e345301d6669f262fe80c16a3d7
laazua <laazua@github.com>
2025-08-08 17:19:27 +0800

    ..

--- linux/bash/scripts/map.sh
+++ linux/bash/scripts/map.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 
+## 方式一
 ips=(
   "1 10.8.0.1"
   "2 10.8.0.2"  
@@ -8,5 +9,18 @@ ips=(
 
 for ip in "${ips[@]}";do
     set -- $ip
-    echo key: $1 value: $2
+    echo key: $1 ++++ value: $2
+done
+
+## 方式二
+declare -A hosts=(
+  ["test"]="10.8.0.1"
+  ["prod"]="10.8.0.2"
+)
+# 获取所有键
+#echo "${!hosts[@]}"
+# 获取所有值
+#echo "${hosts[@]}"
+for key in "${!hosts[@]}";do
+    echo key: $key ++++ value: ${hosts[$key]}
 done
